c0c6c53033b03c6e4a87bb7ecb6d6140e2ff2efc
Dobuzi <kimjw7981@gmail.com>
2020-09-30 17:03:55 +0900

    Initial Commit

---
+++ whatToDo/whatToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatToDoApp.swift
+//  whatToDo
+//
+//  Created by 김종원 on 2020/09/30.
+//
+
+import SwiftUI
+
+@main
+struct whatToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
